206661810974965968b47031fd7002f4145fb6a1
Cameron <cameron@camerons-lion.local>
2013-05-11 18:12:34 +0800

    Initial Commit

---
+++ CMMusicPlayer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CMMusicPlayer
+//
+//  Created by Cameron on 13-5-11.
+//  Copyright (c) 2013年 cm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CMMusicPlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CMMusicPlayer
+//
+//  Created by Cameron on 13-5-11.
+//  Copyright (c) 2013年 cm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
